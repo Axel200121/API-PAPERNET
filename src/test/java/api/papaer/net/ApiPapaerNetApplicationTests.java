f4140e033c5a88bd531c1b97c2bf17839a3626e2
Axel200121 <axelmartinez212001@gmail.com>
2025-02-06 20:45:22 -0600

    creación proyecto

---
+++ src/test/java/api/papaer/net/ApiPapaerNetApplicationTests.java
@@ -0,0 +1,13 @@
+package api.papaer.net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPapaerNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
